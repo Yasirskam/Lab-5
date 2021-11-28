77b72738c051dbfe283d29ebe8fbe9719ee06972
Yasirskam <90673913+Yasirskam@users.noreply.github.com>
2021-11-28 22:37:42 +0200

    Update #22.py

--- #22.py
+++ #22.py
@@ -1,51 +1,51 @@
-импорт  ОС
-filename  =  "tom_sawyer.txt"
-def  g_words ( имя файла ):
-    с  открытым ( имя файла , кодировка = "utf8" ) как  файл :
-        текст  =  файл . читать ()
-    текст  =  текст . replace ( " \ n " , "" )
-    текст  =  текст . replace ( "," , "" ). replace ( "." , "" ). replace ( "?" , "" ). replace ( "!" , "" )
-    текст  =  текст . ниже ()
-    слова  =  текст . разделить ()
-    слова . sort ()
-    ответные  слова
-def  g_chars ( имя файла ):
-    с  открытым ( имя файла , кодировка = "utf8" ) как  файл :
-        chars  =  файл . читать ()
-    chars  =  символы . replace ( " \ n " , "" )
-    chars  =  символы . ниже ()
-    вернуть  символы
+import os
+filename = "tom_sawyer.txt"
+def g_words(filename):
+    with open(filename, encoding="utf8") as file:
+        text = file.read()
+    text = text.replace("\n", " ")
+    text = text.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+    text = text.lower()
+    words = text.split()
+    words.sort()
+    return words
+def g_chars(filename):
+    with open(filename, encoding="utf8") as file:
+        chars = file.read()
+    chars = chars.replace("\n", " ")
+    chars = chars.lower()
+    return chars
 
-def  chars_nospace ( имя файла ):
-    с  открытым ( имя файла , кодировка = "utf8" ) как  файл :
-        chars1  =  файл . читать ()
-    chars1  =  chars1 . replace ( " \ n " , "" )
-    chars1  =  chars1 . ниже ()
-    chars1  =  chars1 . replace ( "" , "" )
-    вернуть  символы1
+def chars_nospace(filename):
+    with open(filename, encoding="utf8") as file:
+        chars1 = file.read()
+    chars1 = chars1.replace("\n", " ")
+    chars1 = chars1.lower()
+    chars1 = chars1.replace(" ", "")
+    return chars1
 
-def  g_words_dict ( слова ):
-    words_dict  =  dict ()
+def g_words_dict(words):
+    words_dict = dict()
  
-    за  словом  в  словах :
-        если  слово  в  words_dict :
-            words_dict [ слово ] =  words_dict [ слово ] +  1
-        еще :
-            words_dict [ слово ] =  1
-    вернуть  words_dict
+    for word in words:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    return words_dict
  
  
-def  main ():
-    filename  =  "tom_sawyer.txt"
-    слова  =  g_words ( имя файла )
-    words_dict  =  g_words_dict ( слова )
-    символы  =  g_chars ( имя файла )
-    characters1  =  chars_nospace ( имя файла )
-    print ( "Качество символов без пропусков:% d"  %  len ( characters1 ))
-    print ( "Кількість символів:% d"  %  len ( символы ))
-    print ( "Количество слів:% d"  %  len ( слова ))
-    print ( "Кількість слів, що не повторються:% d"  %  len ( words_dict ))
+def main(): 
+    filename = "tom_sawyer.txt"
+    words = g_words(filename)
+    words_dict = g_words_dict(words)
+    characters = g_chars(filename)
+    characters1 = chars_nospace(filename) 
+    print("Кількість символів без пропусків: %d" % len(characters1))
+    print("Кількість символів: %d" % len(characters))
+    print("Кількість слів: %d" % len(words))
+    print("Кількість слів, що не повторюються: %d" % len(words_dict))
     
-если  __name__  ==  "__main__" :
-        main ()
-ввод ()
+if __name__ == "__main__":
+        main()
+input()
